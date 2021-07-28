fff5006c73163ed42567342d613e6cecc5f45686
Marina Marques <marina.marqueso@outlook.com>
2021-07-28 19:38:25 -0300

    Adiciona exercicio 10

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio10.js
@@ -0,0 +1,10 @@
+let produto = 100;
+let valorv = 500;
+let imposto = 0.2;
+let custo = produto + produto*imposto;
+let lucro = valorv - custo;
+
+console.log("O custo total é");
+console.log(custo);
+console.log("O lucro será:");
+console.log(lucro);
