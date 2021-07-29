1785a7b8b144afccc0f387c00fbf8b7ee7cee68a
Marina Marques <marina.marqueso@outlook.com>
2021-07-29 15:29:31 -0300

    Adiciona fixacao4, for

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/fixacao4.js
@@ -0,0 +1,6 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index <=3; index++) {
+    let fullList = "Lista completa" + index;
+    console.log(groceryList[index]);
+}
