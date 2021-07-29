7d400504687e77da0bedea43bb627bcfbd0ffc16
Marina Marques <marina.marqueso@outlook.com>
2021-07-29 19:05:01 -0300

    Adiciona exerc5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -5,12 +5,22 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // let soma = numbers[0] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
 // console.log(soma);
 
-let soma = numbers[0] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
-let media = soma / 10;
-console.log(media);
+// let soma = numbers[0] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+// let media = soma / 10;
+// console.log(media);
 
-if (media > 20) {
-    console.log("Valor Maior que 20");
-} else {
-    console.log("Valor menor ou igual a 20");
+// if (media > 20) {
+//     console.log("Valor Maior que 20");
+// } else {
+//     console.log("Valor menor ou igual a 20");
+// }
+
+let maiorNumber = 0;
+
+for (let index = 0; index < numbers.length; index ++) {
+    let number = numbers[index];
+    if (number > maiorNumber) {
+        maiorNumber = number;
+        }
 }
+console.log(maiorNumber);
