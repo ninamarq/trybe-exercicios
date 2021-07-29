364ea8a5bd1988bf3b8de61eef3c22c9b3da212f
Marina Marques <marina.marqueso@outlook.com>
2021-07-29 18:14:50 -0300

    Adiciona exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio.js
@@ -5,3 +5,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // let soma = numbers[0] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
 // console.log(soma);
 
+let soma = numbers[0] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+let media = soma / 10;
+console.log(media);
