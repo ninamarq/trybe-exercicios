9d1fddbd857980da7f378e42b742beb11dcc8819
Marina Marques <marina.marqueso@outlook.com>
2021-08-02 15:02:34 -0300

    Adiciona fixacao1 alguns exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao1.js
@@ -0,0 +1,12 @@
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: {
+        golden: 2,
+        silver: 3,
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+    }
+}
+console.log("A jogadora " + player.name + " tem " + player.age + " anos de idade");
+console.table(player);
