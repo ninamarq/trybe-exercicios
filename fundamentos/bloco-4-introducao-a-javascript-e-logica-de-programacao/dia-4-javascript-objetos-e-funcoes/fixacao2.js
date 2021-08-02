1055d3573219198ace62ba509de7445af93895a8
Marina Marques <marina.marqueso@outlook.com>
2021-08-02 15:21:09 -0300

    Adiciona resolucao fixacao2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao2.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao2.js
@@ -1,9 +1,19 @@
-let names = {
-    person1: 'João',
-    person2: 'Maria',
-    person3: 'Jorge' 
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge' 
+//   };
+
+//   for (let name in names) {
+//       console.log("Olá " + names[name]);
+//   }
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
   };
 
-  for (let name in names) {
-      console.log("Olá " + names[name]);
-  }
+for (let key in car) {
+    console.log(key + " - " + car[key]);
+}
