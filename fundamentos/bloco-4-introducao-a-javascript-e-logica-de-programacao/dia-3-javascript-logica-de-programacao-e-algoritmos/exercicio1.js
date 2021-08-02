a2d9795f8923459d3847a2f7504da3436ac9d2d1
Marina Marques <marina.marqueso@outlook.com>
2021-08-01 23:06:32 -0300

    Concatena e conclui exerc1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -1,9 +1,13 @@
 // receber tamanho do quadrado
 let num = 5;
 
-// imprimir * para tamanho do quadrado linha
+// // imprimir * para tamanho do quadrado linha
 for (let i = 0; i < num; i++) {
-    let j = 
-    
+    let linha = "";
+    for (let j = 0; j<num; j++){
+//imprimir * para tam quadrado coluna       
+  linha = linha + "*";
+        
+    }
+    console.log(linha);    
 }
-//imprimir * para tam quadrado coluna
