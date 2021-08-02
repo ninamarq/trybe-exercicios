b5e28e484f6f6cb0f9a55b2701dbc6a230d19c08
Marina Marques <marina.marqueso@outlook.com>
2021-08-01 23:26:32 -0300

    Adiciona exerc2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,12 @@
+let num = 5;
+
+// para cada index, imprime *
+
+for (let i = 0; i < num; i++){
+    let linha = "";
+    for (let j = 0; j <= i ; j++){
+        linha = linha + "*";
+        
+        }
+    console.log(linha);
+}
