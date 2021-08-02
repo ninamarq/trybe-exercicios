55ff051413f6c09bab0e21134d192b260e0e0859
Marina Marques <marina.marqueso@outlook.com>
2021-08-02 19:25:59 -0300

    Resolucao exercicio 1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exerciciofor-in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exerciciofor-in.js
@@ -3,4 +3,31 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
-  console.log("Bem-vinda, " + info.personagem);
+  info['recorrente'] = 'Sim';
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
+  };
+  
+  //console.log(info);
+  //console.log("Bem-vinda, " + info.personagem);
+
+//   for (let key in info) {
+//       console.log(key);
+//   }
+  
+//   for (let key in info) {
+//       console.log(info[key]);
+//   }
+let infomRec = [info.personagem, info.origem , info.nota];
+let info2mRec = [info2.personagem, info2.origem, info.nota];
+
+for(let key in infomRec&&info2mRec){
+    console.log(infomRec[key] + " e " + info2mRec[key]);
+}if (info.recorrente === info2.recorrente) {
+
+    console.log("Ambos recorrentes");
+}
