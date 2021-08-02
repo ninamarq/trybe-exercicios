77dd1704b071be73e2d13b3383e2ae0c10ce9708
Marina Marques <marina.marqueso@outlook.com>
2021-08-02 15:05:48 -0300

    Adiciona resolucao fixacao

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao1.js
@@ -2,11 +2,11 @@ let player = {
     name: 'Marta',
     lastName: 'Silva',
     age: 34,
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
     medals: {
         golden: 2,
-        silver: 3,
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+        silver: 3,}
     }
-}
+
 console.log("A jogadora " + player.name + " tem " + player.age + " anos de idade");
-console.table(player);
+console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes")
