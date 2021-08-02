d09e9df3a8eb700885e689b411657c32863ff640
Marina Marques <marina.marqueso@outlook.com>
2021-08-02 14:58:19 -0300

    adiciona diretorios do dia

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio4.js
@@ -3,13 +3,3 @@ let num = 10;
 // comparando a base, a linha acima sempre será -2 (-1 por lado).
 // j == num
 // i == (n+1)/2
-for ( i = 0; i <= Math.ceil(num/2); i++){
-    let linha = "";
-    for(j = 0; j <= num; j++){
-        if(j >= (num-(i-1)) && j <= (num+(i-1))){
-            linha = linha + "*";
-        } else{
-            linha = linha + " ";
-        }
-    }console.log(linha);
-}
