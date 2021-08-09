f301f57b0c4451ff37c22f130ef5655b0bc86a6c
Marina Marques <marina.marqueso@outlook.com>
2021-08-09 19:10:09 -0300

    Adiciona resolução com muito esforço haha

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,22 +1,29 @@
-        /*
-        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-        - document.getElementById()
-        - document.getElementsByClassName()
-        - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
-        */
+//  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+//  Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+//         */
 
-function textP() {
-        document.getElementById("parag").innerText = "Brincando com o getElementById";
-}
-function fontColor() {
-        document.getElementById("parag").color = rgb(76,164,109);
-}
-function squareColor() {
-        document.getElementsByClassName("main-content center-content").background-color === white; 
-}
+// function textP() {
+//         return document.getElementById("parag").innerText = "Brincando com o getElementById";
+// }
+// function fontColor() {
+//         return document.getElementById("parag").color = rgb(76,164,109);
+// }
+// function squareColor() {
+//         return document.getElementsByClassName("main-content center-content").backgroundcolor = white; 
+// }
+// function textCorrection() {
+//         return document.getElementsByTagName("h1").innerText = "Exercício 5.1 - JavaScript";
+// }
+// function pMajorText() {
+//         let pUpperCase = document.getElementsByTagName("p");
+//         return pUpperCase.toUpperCase();
+// }
+// function allPTag() {
+//         document.querySelectorAll("p");
+// }
+
+document.getElementById('parag').innerText = "Brincando com o getElementById";
+document.getElementById('parag').style.color = 'rgb(76,164,109)';
+document.getElementsByClassName('title')[0].innerText = "Exercício 5.1 - JavaScript";
+document.getElementsByTagName('p');
+document.getElementsByClassName('center-content')[0].style.backgroundColor = 'white';
