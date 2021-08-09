1cacaeb15bb3a103e9ab00d765b68dafd3f76c62
Marina Marques <marina.marqueso@outlook.com>
2021-08-09 15:17:46 -0300

    Adiciona exercicios de fixacao 1 5.1

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao1.js
@@ -0,0 +1,3 @@
+document.getElementById("page-title").innerText = "Spider-man";
+document.getElementById("second-paragraph").innerText = "Usando criatividade, hoje ta difícil criar haha";
+document.getElementById("subtitle").innerText = "Amo todos do homem aranha"
