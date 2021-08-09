65cb1cfa27084d6de713f90b18f9036b797755d5
Marina Marques <marina.marqueso@outlook.com>
2021-08-09 15:33:57 -0300

    Fixacao 2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao1.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fixacao1.js
@@ -1,3 +1,9 @@
-document.getElementById("page-title").innerText = "Spider-man";
-document.getElementById("second-paragraph").innerText = "Usando criatividade, hoje ta difícil criar haha";
-document.getElementById("subtitle").innerText = "Amo todos do homem aranha"
+// document.getElementById("page-title").innerText = "Spider-man";
+// document.getElementById("second-paragraph").innerText = "Usando criatividade, hoje ta difícil criar haha";
+// document.getElementById("subtitle").innerText = "Amo todos do homem aranha"
+
+let p = document.getElementsByClassName("paragraph");
+
+for (let i = 0; i < p.length; i += 1) {
+    p[i] = p[i].innerText = "oi";
+}
