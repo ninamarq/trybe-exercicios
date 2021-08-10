7708bbb48b3d2e74bc95c807a6f5b2db3d910119
Marina Marques <marina.marqueso@outlook.com>
2021-08-10 15:48:25 -0300

    Adiciona mais elementos js para a fixacao1 e cria fixacao2

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao1.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao1.js
@@ -1,2 +1,3 @@
 document.getElementById("elementoOndeVoceEsta");
 document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "green";
+document.getElementById("pai").nextElementSibling;
