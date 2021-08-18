86333842142943b2901059ffa962ec4ed350c7e2
Marina Marques <marina.marqueso@outlook.com>
2021-08-18 19:29:25 -0300

    Adiciona options estados brasileiros

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,1 +1,8 @@
 let selectTag = document.querySelector('select');
+let estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO"];
+
+for (let i = 0; i < estados.length; i++) {
+    let optionTag = document.createElement('option');
+    optionTag.innerText = estados[i];
+    selectTag.appendChild(optionTag);
+}
